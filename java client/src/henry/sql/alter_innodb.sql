8775db17c1d02de04dd042971b7dba3140cc5a9a
han <qihan.dev@gmail.com>
2012-05-11 18:46:22 -0500

    a

--- java client/src/henry/sql/alter_innodb.sql
+++
@@ -1,7 +0,0 @@
-alter table productos ENGINE=InnoDB;
-alter table ingresos ENGINE=InnoDB;
-alter table ingreso_items ENGINE=InnoDB;
-alter table notas_de_venta ENGINE=InnoDB;
-alter table ordenes_de_despacho ENGINE=InnoDB;
-alter table items_de_despacho ENGINE=InnoDB;
-alter table items_de_venta ENGINE=InnoDB;
--- java client/src/henry/sql/delete.sql
+++
@@ -1,8 +0,0 @@
-drop table items_de_factura;
-drop table factura;
-drop table items_de_venta;
-drop table notas_de_venta;
-drop table productos;
-drop table usuarios;
-drop table clientes;
-
