d52a9f0a4fdbc78c4edcb5a0f306e59a2a17853e
Han <qihan.dev@gmail.com>
2013-12-14 21:06:22 -0800

    Modified setup.sh

--- servidor/setup.sh
+++ servidor/setup.sh
@@ -1,10 +1,4 @@
 apt-get install mysql-server mysql-client
-apt-get install nginx
-apt-get install python-virtualenv python-pip fabric
-#create virtual env
-cd $DEPLOYMENT_ROOT
-virtualenv env
-source env/bin/activate
-pip install django-1.4.0
-pip install uwgsi
+apt-get install python-virtualenv python-pip
+apt-get install uwsgi uwsgi-plugin-python nginx
 
