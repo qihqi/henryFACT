fa37c24e458235f3371cc4ea4853eb92e024679a
servidor <servidor@servidor-centro.(none)>
2013-12-28 10:32:52 -0500

    static files

--- servidor/henry/get_all_invoice.py
+++ servidor/henry/get_all_invoice.py
@@ -5,34 +5,36 @@ import settings
 setup_environ(settings)
 
 from facturas.models import *
+import json
+from datetime import datetime
 
 def _as_centavo(s):
     return int(s * 100)
 
-
-all_despacho = OrdenDeDespacho.objects.all()
-for desp in all_despacho:
-    print json.dumps(dump_item(desp))
-
 def dump_item(desp):
     desp_dict = {
         'codigo': desp.codigo,
-        'bodega': desp.bodega,
-        'vendedor': desp.vendedor,
-        'cliente':desp.cliente,
+        'bodega': desp.bodega_id,
+        'vendedor': desp.vendedor_id,
+        'cliente':desp.cliente_id,
         'fecha':desp.fecha.isoformat(),
         'total': _as_centavo(desp.total),
-        'desc': _as_centavo(desp.desc),
-        'pago':_as_centavo(desp.pago),
-        'precio_modificado': desc.precio_modificado
+        'desc': _as_centavo(desp.desc) if desp.desc else None,
+        'pago': desp.pago,
+        'precio_modificado': desp.precio_modificado,
         'items' : []
     }
     for item in ItemDeDespacho.objects.filter(desp_cod_id=desp.id):
         desp_dict['items'].append({
-            'producto': item.producto,
+            'producto': item.producto_id,
             'precio': _as_centavo(item.precio),
             'cantidad': int(item.cantidad * 1000),
             'precio_modificado': item.precio_modificado
         })
     return desp_dict
+
+
+all_despacho = OrdenDeDespacho.objects.filter(fecha__gte=datetime(2013, 11,11))
+for desp in all_despacho:
+    print json.dumps(dump_item(desp))
 
--- servidor/henry/static/row.js
+++ servidor/henry/static/row.js
@@ -1,27 +1,82 @@
+function is_number(b) {
+    return b!=undefined && b!=null && (b - 0) == b;
+}
+
+function popup(){
+    
+	var newwindow=window.open("/static/buscar_producto.html",'name','height=700,width=500, scrollbars=yes');
+    window.codigo_click = $(this).attr("id");
+    if (window.focus) 
+        newwindow.focus();
+	return false;
+}
+
+
 
 var count=0;
 function getRow() {
-    var p = $("<p>");
-    var codigo = $("<input name=\"codigo\">");
-    var cant = $("<input name=\"cant\">");
-    var nombre = $("<span id=\"here\"" + count + " name=\"nombre\">");
-    p.append(codigo, cant, nombre);
+    var p = $("<tr>");
+    var codigo_cell = $("<td>");
+    var cant_cell = $("<td>");
+    var nombre_cell = $("<td>");
+    var buscar_cell = $("<td>");
+    var trans_cell = $("<td>");
+    var codigo = $("<input id=\"here"+ count + "\" name=\"codigo\" class=\"text_field\">");
+    var cant = $("<input id=\"here"+ count + "\" name=\"cant\" class=\"text_field\">");
+    var nombre = $("<span id=\"here"+ count + "\" name=\"nombre\" class=\"text_field\">");
+    var buscar = $("<a id=\"here"+ count + "\" name=\"nombre\" href=\"\" class=\"text_field\" >");
+   // var trans = $("<input id=\"here"+ count + "\" name=\"transform\" class=\"text_field\" type=\"checkbox\">");
+    buscar.click(popup);
+    buscar.html("buscar"); 
+    codigo_cell.append(codigo);
+    cant_cell.append(cant);
+    nombre_cell.append(nombre);
+    buscar_cell.append(buscar);
+    //trans_cell.append(trans);
+    p.append(buscar_cell, codigo_cell, cant_cell, nombre_cell);
 
     codigo.keypress(function(event) {
         if (event.which == 13){
             event.preventDefault();
-            cant.focus();
+            var id = $(this).attr("id");
+            var codigo = $(this).val();
+            var bodega = $("#id_bodega").val();
+            var data = "codigo=" + codigo + "&bodega=" + bodega;
+            $.ajax( {
+                    url:"/r/producto/get_prod_cont", 
+                    data : data,
+                    success:function(result){
+                        if (result.status) {
+                            $("span#"+id).html(result.nombre);
+                            cant.focus();
+                        }
+                        else {
+                            $("span#"+id).html("Codigo Equivocado");
+                            $(this).select();
+                        }
+                    }
+            });
         }
     });
 
     cant.keypress(function(event) {
         if (event.which == 13) {
             event.preventDefault();
+            
+            var number = $(this).val();
+            if (! is_number(number))
+            { 
+                alert("cantidad debe ser numero");
+                return;
+            }
+
             var a = getRow();
-            for (var i = 0; i < 3; i++)
-                $(this).parent().append(a[i]);
+            $("#insert").append(a);
+            a.beginning.focus();
+
         }
     });
-    count++; 
+    count++;
+    p.beginning = codigo;
     return p;
 }
